805ba2809abcb79b8b0d6a8ce1da73579f304e32
Marta Sousa <marta.sousa@gmail.com>
2020-01-16 19:35:11 -0300

    substituindo código de criação de nova tarefa pela função de gerar nova tarefa

--- js/script.js
+++ js/script.js
@@ -17,27 +17,7 @@ buttonAdd.onclick = function(){
   let valorDigitado = inputAdd.value
   listaTarefas.push(valorDigitado)
 
-  // Utilizar a função gerar tarefa aqui
-  let tarefa = document.createElement('div')
-  tarefa.setAttribute('class', 'tarefa')
-
-  let titulo = document.createElement('div')
-  titulo.setAttribute('class', 'col-md-8 col-10')
-  titulo.textContent = valorDigitado
-
-  let buttonSpace = document.createElement('div')
-  buttonSpace.setAttribute('class', 'col-md-2')
-
-  let checkbox = document.createElement('input')
-  checkbox.setAttribute('type', 'checkbox')
-
-  buttonSpace.appendChild(checkbox)
-  tarefa.appendChild(titulo)
-  tarefa.appendChild(buttonSpace)
-
-  board.appendChild(tarefa)
-  //Fim da função
-
+  gerarTarefa(valorDigitado)
 
   //Sobreescrever as informações que estavam no localStorage
   localStorage.setItem('listaTarefas', JSON.stringify(listaTarefas))
